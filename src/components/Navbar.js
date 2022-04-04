1feb826c4ed93bb743e3cd0b861aaff2c4cf6051
Viktorija Kuneviciute <viktorija.qun@gmail.com>
2022-04-04 10:39:42 +0200

    homepage and navbar, add todos

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import './Navbar.css'
+const Navbar = () => {
+    return (
+        
+     <>
+         <nav className="navbar">
+             <div>  </div>
+             <ul className="list">
+                 <li className="list-item"> </li>
+                 <li className="list-item"></li>
+                 <li className="list-item"></li>
+
+             </ul>
+         </nav>
+     </>
+
+    )
+}
+export default Navbar;
